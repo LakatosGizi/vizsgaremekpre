2bbe716e428d162a3bd5945a992511233b832832
LakatosGizi <alexszombat44@gmail.com>
2025-11-07 10:17:29 +0100

    atlagolas kesz

--- main.py
+++ main.py
@@ -5,11 +5,18 @@ with open("data.txt" , "r" , encoding="utf-8") as fin:
         adatok.append(int(sor))
 print(adatok)
 # adatok átlaga 2.
-
+atlag=sum(adatok)/len(adatok)
+print(f"a beolvasott adatok átlaga: {atlag: .2f}")
 # döntsűk el hogy volt e négyes 3.
 
 # keresűk meg hogy volt e ötös 4.
+with open("jegyek.txt", "r", encoding="utf-8") as fin:
+    jegyek =()
 
+if 5 in jegyek:
+    print("Volt ötös!")
+else:
+    print("Nem volt ötös.")
 # hány darab kilences volt 5.
 
 # mennyi a legnagyobb beirt szám 6.
